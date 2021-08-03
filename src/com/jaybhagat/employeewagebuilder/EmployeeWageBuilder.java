a14c742f8b4aa0c9acaf2ea8fa0af1cf6a0da2bf
unknown <RockstarJay>
2021-08-03 14:21:00 +0530

    Added: check employee present or absent

--- src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
@@ -1,8 +1,22 @@
 package com.jaybhagat.employeewagebuilder;
 
+/**
+ * class to check the attendance of employee
+ * @author com.jaybhagat
+ */
+
 public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
+		
+		int IS_PRESENT = 1;
+		double emp_Check = (int) (Math.floor(Math.random() * 10)) % 2;    // generates two random numbers either 0 or 1
+		if (emp_Check == IS_PRESENT) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
