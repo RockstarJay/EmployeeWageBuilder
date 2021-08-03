55a82a4c39d4a6d9a9ed691d845ae824372a7aea
unknown <RockstarJay>
2021-08-03 14:36:16 +0530

    Added: calculate daily wage of employee

--- src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
@@ -1,7 +1,7 @@
 package com.jaybhagat.employeewagebuilder;
 
 /**
- * class to check the attendance of employee
+ * class to check the attendance and calculate the daily wage of employee
  * @author com.jaybhagat
  */
 
@@ -11,12 +11,18 @@ public class EmployeeWageBuilder {
 		System.out.println("Welcome to Employee Wage Computation");
 		
 		int IS_PRESENT = 1;
+		int WagePerHour = 20;
+		int DayHour = 0;
+		int DailyWage = 0;
 		double emp_Check = (int) (Math.floor(Math.random() * 10)) % 2;    // generates two random numbers either 0 or 1
 		if (emp_Check == IS_PRESENT) {
 			System.out.println("Employee is Present");
+			DayHour = 8;
 		}
 		else {
 			System.out.println("Employee is Absent");
 		}
+		DailyWage = WagePerHour * DayHour;     // calculate daily wage
+		System.out.println("Employee Daily Wage is: " +DailyWage);
 	}
 }
