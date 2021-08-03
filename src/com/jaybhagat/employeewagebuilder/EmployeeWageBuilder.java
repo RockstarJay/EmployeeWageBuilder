d9178afbbcd372d2d8cf508719687f0edf89916e
unknown <RockstarJay>
2021-08-03 15:08:40 +0530

    Added: wage computation for part time employee

--- src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
@@ -1,7 +1,8 @@
 package com.jaybhagat.employeewagebuilder;
 
 /**
- * class to check the attendance and calculate the daily wage of employee
+ * class to check the attendance of employee if he present for full time, half time or absent 
+ * calculate the daily wage of employee depending on attendance
  * @author com.jaybhagat
  */
 
@@ -10,19 +11,24 @@ public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 		
-		int IS_PRESENT = 1;
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int WagePerHour = 20;
 		int DayHour = 0;
 		int DailyWage = 0;
-		double emp_Check = (int) (Math.floor(Math.random() * 10)) % 2;    // generates two random numbers either 0 or 1
-		if (emp_Check == IS_PRESENT) {
-			System.out.println("Employee is Present");
+		double emp_Check = (int) (Math.floor(Math.random() * 10)) % 3;    // generates three random numbers either 0 or 1 or 2
+		if (emp_Check == IS_FULL_TIME) {
+			System.out.println("Employee is Present Full Time");
 			DayHour = 8;
+		}
+		else if (emp_Check == IS_PART_TIME) {
+			System.out.println("Employee is Present Part Time");
+			DayHour = 4;
 		}
 		else {
 			System.out.println("Employee is Absent");
 		}
-		DailyWage = WagePerHour * DayHour;     // calculate daily wage
+		DailyWage = WagePerHour * DayHour;                         // calculate daily wage
 		System.out.println("Employee Daily Wage is: " +DailyWage);
 	}
 }
