63f7d18a4c17ea5587d8901818d1417d152c3582
unknown <RockstarJay>
2021-08-03 15:50:31 +0530

    Added: employee wage for a month

--- src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
@@ -3,6 +3,7 @@ package com.jaybhagat.employeewagebuilder;
 /**
  * class to check the attendance of employee if he present for full time, half time or absent using switch case
  * calculate the daily wage of employee depending on attendance
+ * adding daily wage to calculate  monthly wage
  * @author com.jaybhagat
  */
 
@@ -14,26 +15,31 @@ public class EmployeeWageBuilder {
 		final int IS_FULL_TIME = 1;
 		final int IS_PART_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
+		final int MONTHLY_WORKING_DAYS = 20;
 		
 		int dayHour = 0;
 		int dailyWage = 0;
-		double emp_Check = (int) (Math.floor(Math.random() * 10)) % 3;    // generates three random numbers either 0 or 1 or 2
-		
-		switch ((int)emp_Check) {
-			case IS_FULL_TIME:
-				System.out.println("Employee is Present Full Time");
-				dayHour = 8;
-				break;
-				
-			case IS_PART_TIME:
-				System.out.println("Employee Is Present Part Time");
-				dayHour = 4;
-				break;
-				
-			default:
-				System.out.println("Employee Is Absent");
+		int monthlyWage = 0;
+		for (int i = 0; i < MONTHLY_WORKING_DAYS; i++) {
+			double emp_Check = (Math.floor(Math.random() * 10)) % 3;    // generates three random numbers either 0 or 1 or 2
+			
+			switch ((int)emp_Check) {
+				case IS_FULL_TIME:
+					System.out.println("Employee is Present Full Time");
+					dayHour = 8;
+					break;
+					
+				case IS_PART_TIME:
+					System.out.println("Employee is Present Part Time");
+					dayHour = 4;
+					break;
+					
+				default:
+					System.out.println("Employee is Absent");
+			}
+			dailyWage = WAGE_PER_HOUR * dayHour;                  // calculate daily wage
+			monthlyWage += dailyWage;                             // adding into monthly wage
 		}
-		dailyWage = WAGE_PER_HOUR * dayHour;                         // calculate daily wage
-		System.out.println("Employee Daily Wage is: " +dailyWage);
+		System.out.println("\nMonthly Employee Wage is: " +monthlyWage);
 	}
 }
