5763fab456b31ab8aac26722075e1e475d6ec901
unknown <RockstarJay>
2021-08-03 16:12:05 +0530

    Added: wage till working hours are 100 or working days are 20

--- src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
@@ -3,7 +3,9 @@ package com.jaybhagat.employeewagebuilder;
 /**
  * class to check the attendance of employee if he present for full time, half time or absent using switch case
  * calculate the daily wage of employee depending on attendance
- * adding daily wage to calculate  monthly wage
+ * calculating working hours to compare with monthly working hours
+ * looping till monthly working days or working hours reached to required
+ * adding daily wage to calculate monthly wage and showing result
  * @author com.jaybhagat
  */
 
@@ -16,13 +18,15 @@ public class EmployeeWageBuilder {
 		final int IS_PART_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
 		final int MONTHLY_WORKING_DAYS = 20;
+		final int MONTHLY_WORKING_HOURS = 100;
 		
 		int dayHour = 0;
 		int dailyWage = 0;
 		int monthlyWage = 0;
+		int workingHours = 0;
 		
-		for (int i = 0; i < MONTHLY_WORKING_DAYS; i++) {
-			double emp_Check = (Math.floor(Math.random() * 10)) % 3;    // generates three random numbers either 0 or 1 or 2
+		for (int i = 0; i < MONTHLY_WORKING_DAYS && workingHours < MONTHLY_WORKING_HOURS; i++) {
+			double emp_Check = (Math.floor(Math.random() * 10)) % 3;       // generates three random numbers either 0 or 1 or 2
 			
 			switch ((int)emp_Check) {
 				case IS_FULL_TIME:
@@ -38,8 +42,9 @@ public class EmployeeWageBuilder {
 				default:
 					System.out.println("Employee Is Absent");
 			}
-			dailyWage = WAGE_PER_HOUR * dayHour;                         // calculate daily wage
-			monthlyWage += dailyWage;                                    // adding into monthly wage
+			dailyWage = WAGE_PER_HOUR * dayHour;                  // calculate daily wage
+			monthlyWage += dailyWage;                             // adding into monthly wage
+			workingHours += dayHour;                              // calculating working hours to compare with monthly working hours 
 		}	
 		System.out.println("\nMonthly Employee Wage is: " +monthlyWage);
 	}
