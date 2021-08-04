35c1df11fa239ae1405a4bfcd47a6dccf12dc31a
Jay Bhagat <43294104+RockstarJay@users.noreply.github.com>
2021-08-04 14:04:48 +0530

    Added: wage computation for part time employee

--- src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/jaybhagat/employeewagebuilder/EmployeeWageBuilder.java
@@ -13,22 +13,22 @@ public class EmployeeWageBuilder {
 		
 		int IS_FULL_TIME = 1;
 		int IS_PART_TIME = 2;
-		int WagePerHour = 20;
-		int DayHour = 0;
-		int DailyWage = 0;
-		double emp_Check = (int) (Math.floor(Math.random() * 10)) % 3;    // generates three random numbers either 0 or 1 or 2
-		if (emp_Check == IS_FULL_TIME) {
+		int wagePerHour = 20;
+		int dayHour = 0;
+		int dailyWage = 0;
+		double empCheck = (int) (Math.floor(Math.random() * 10)) % 3;    // generates three random numbers either 0 or 1 or 2
+		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is Present Full Time");
-			DayHour = 8;
+			dayHour = 8;
 		}
-		else if (emp_Check == IS_PART_TIME) {
+		else if (empCheck == IS_PART_TIME) {
 			System.out.println("Employee is Present Part Time");
-			DayHour = 4;
+			dayHour = 4;
 		}
 		else {
 			System.out.println("Employee is Absent");
 		}
-		DailyWage = WagePerHour * DayHour;                         // calculate daily wage
-		System.out.println("Employee Daily Wage is: " +DailyWage);
+		dailyWage = wagePerHour * dayHour;                         // calculate daily wage
+		System.out.println("Employee Daily Wage is: " +dailyWage);
 	}
 }
